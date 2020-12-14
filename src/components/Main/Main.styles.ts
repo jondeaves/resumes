15f837db9bfa7c2a636ad9d7366de7e2a25d3773
Jon Deaves <hello@jondeaves.me>
2020-12-14 22:47:50 +0000

    chore: faaaaaade

--- src/components/Main/Main.styles.ts
+++ src/components/Main/Main.styles.ts
@@ -3,7 +3,9 @@ import { padding } from 'polished';
 import { MainStyles } from './Main.types';
 
 const styles: MainStyles = {
-  main: ({ theme }) => ({}),
+  main: ({ theme }) => ({
+    // background: `linear-gradient(180deg, ${theme.color.grey} 0%, ${theme.color.grey} 90%, #ffffff 100%)`,
+  }),
 
   blurb: ({ theme }) => ({
     marginBottom: theme.spacing.medium,
